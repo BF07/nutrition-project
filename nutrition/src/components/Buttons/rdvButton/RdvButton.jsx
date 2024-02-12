b94e1076b572ab43a0963c738e52809c1b3fa3b7
Bruno Fina <bruno.fina07@gmail.com>
2024-02-12 15:34:05 +0400

    Create rdvButton component

---
+++ nutrition/src/components/Buttons/rdvButton/RdvButton.jsx
@@ -0,0 +1,11 @@
+import './rdvButton.css'
+
+export default function RdvButton () {
+    
+
+    return <>
+    <section className='rdv-container'>
+        <button>Prendre rendez-vous en ligne</button>
+    </section>
+    </>
+}
